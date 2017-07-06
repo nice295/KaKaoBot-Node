dbd8ad47088deb2686d047dc79169c9ea3f9b9bf
Horace Kyuho Lee <KyuhoLee@Leeui-MacBook-Pro.local>
2017-07-06 23:35:22 +0900

    make it better

--- service/BotService.js
+++ service/BotService.js
@@ -14,7 +14,7 @@ const getMenu = require('../service/getMenu');
 Bot.choseMenu = (req, content, callback) => {  
   
   switch (content) {
-    case "😍 1식당-점심":
+    case "🏠 1식당-점심":
        getMenu(12, function(data) {
         callback(null, message.baseType(data));
       });
